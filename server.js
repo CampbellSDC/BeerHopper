4fe3c157c81eddf3b112fa03e0739bb3525bc99d
Steve <scampbellatc@gmail.com>
2023-03-20 10:56:25 -0400

    adding MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const mongoose = require('mongoose')
+
 
 
 app.listen(PORT, () => console.log(`Listening at port ${PORT}`))
